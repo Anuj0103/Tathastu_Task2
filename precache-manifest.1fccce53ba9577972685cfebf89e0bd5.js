4550a5bf50b9ed833432066b76f9a904e1f9949d
Anuj0103 <Anuj@0103>
2021-08-01 23:18:44 +0530

    Updates

---
+++ precache-manifest.1fccce53ba9577972685cfebf89e0bd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6afd4cbcad70d7b5716b75ab2dc597c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "181cc2e5fabb99b86ce4",
+    "url": "/static/css/main.cca13aff.chunk.css"
+  },
+  {
+    "revision": "deae42d8de7a2e23c23e",
+    "url": "/static/js/2.0570ad0a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.0570ad0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "181cc2e5fabb99b86ce4",
+    "url": "/static/js/main.adb80547.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
